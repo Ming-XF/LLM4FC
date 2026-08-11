4fe8e4d467096815a73e8b3de29cf57527221dae
Ming-XF <3421891093@qq.com>
2026-08-11 20:28:07 +0800

    跑出了Lora和GCLora实验结果，但几乎都没有提升

--- scripts/train_TimeLLM.sh
+++ scripts/train_TimeLLM.sh
@@ -42,11 +42,10 @@ deepspeed --num_gpus=6 main.py \
     --use_dataset_prompt \
     --use_task_prompt \
     --use_lora \
-    --use_gc_lora \
     --lora_rank 16 \
     --lora_alpha 32 \
     --lora_dropout 0.1 \
-    --lora_target_modules "o_proj" \
+    --lora_target_modules "q_proj,v_proj" \
     --early_stop_patience 10 \
     --early_stop_min_delta 0.001 \
     --early_stop_metric "$EARLY_STOP_METRIC" \
